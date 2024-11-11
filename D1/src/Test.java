c8c01f86d6511c6924cc52a0a504ade11c79849a
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-11-11 16:02:57 +0100

    implement logic of account class to add, withdraw and transfer money

--- D1/src/Test.java
+++ D1/src/Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account account1 = new Account("1", 12345);
+        Account account2 = new Account("2", 67890);
+
+        account1.Add(100);
+        account1.Withdraw(50);
+        account1.Transfer(200, account2);
+
+        System.out.println("Account 1 balance: " + account1.ShowCurrentAmount());
+        System.out.println("Account 2 balance: " + account2.ShowCurrentAmount());
     }
 }
