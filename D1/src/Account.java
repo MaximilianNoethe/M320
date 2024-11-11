4a82a9334b66e1520a03ed73352b31a69f66f5a5
aryanbisen <aryan.bisen@edu.tbz.ch>
2024-11-11 15:57:55 +0100

    Implement account class

--- D1/src/Account.java
+++ D1/src/Account.java
@@ -1,3 +1,28 @@
 public class Account {
+    private String accountNumber;
+    private double saldo;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.saldo = 100;
+    }
+    public void Add(double amount){
+        saldo += amount;
+    };
+    public void Withdraw (double amount){
+        saldo -= amount;
+    }
+    public boolean transfer(double amount, Account targetAccount) {
+        if (amount > 0 && amount <= saldo) {
+            this.Withdraw(amount);
+            targetAccount.Add(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public double ShowCurrentAmount(){
+        return saldo;
+    }
 
 }
