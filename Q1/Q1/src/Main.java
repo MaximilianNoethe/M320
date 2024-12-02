4151bd9850a5c040b072c6446888d8c6e13a33b6
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-12-02 13:45:40 +0100

    Implement Q2.

---
+++ Q1/Q1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        GuessTheNumberGame guessTheNumberGame = new GuessTheNumberGame();
+        guessTheNumberGame.playGame();
+    }
+}
